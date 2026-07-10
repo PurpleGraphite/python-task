f15d3bfa06453e22caae0c0b6be77c7c96623705
Aditya83-byte <91058321+Aditya83-byte@users.noreply.github.com>
2026-07-10 14:19:12 +0530

    feat: add BookSale model

--- book_sales/models.py
+++ book_sales/models.py
@@ -1,7 +1,12 @@
+from datetime import date
+
 from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     author = models.CharField(max_length=50, db_index=True)
 
-
+class BookSale(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    sale_date = models.DateField(default=date.today)
+    quantity = models.PositiveIntegerField()
