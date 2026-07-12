330e2223780e8abb51dc7f4fadbbe9a245e25b0e
Aditya83-byte <91058321+Aditya83-byte@users.noreply.github.com>
2026-07-12 16:36:19 +0530

    fix: remove misplaced DRF configuration

--- python_task/urls.py
+++ python_task/urls.py
@@ -21,8 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("book_sales.urls")),
 ]
-
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
-}
