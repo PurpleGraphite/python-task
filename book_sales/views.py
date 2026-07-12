d40a00379e07bc4589f4772a2a2278db828dfdcd
Aditya83-byte <91058321+Aditya83-byte@users.noreply.github.com>
2026-07-12 14:01:18 +0530

    feat: add book list and create endpoint

--- book_sales/views.py
+++ book_sales/views.py
@@ -1,3 +1,12 @@
 from django.views.generic import TemplateView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    DestroyAPIView,
+)
 class BookSalesDashboardView(TemplateView):
     template_name = "book_sales/book_sales.html"
+class BookListCreateView(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    pagination_class = None
