19621271a5f611361a97890d2539aeee404c9bfd
Aditya83-byte <91058321+Aditya83-byte@users.noreply.github.com>
2026-07-12 13:58:23 +0530

    feat: add dashboard template view

--- book_sales/views.py
+++ book_sales/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import TemplateView
+class BookSalesDashboardView(TemplateView):
+    template_name = "book_sales/book_sales.html"
