197f1458f558fce418d0b5b4c9cfc30e3a83c45a
Aditya83-byte <91058321+Aditya83-byte@users.noreply.github.com>
2026-07-12 15:52:49 +0530

    feat: register book sales URL routes

--- python_task/urls.py
+++ python_task/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("book_sales.urls")),
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+}
